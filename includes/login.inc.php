a5d0c64cc12f817df14e9101cae831a9fd9e2d24
HollowHero <31659720+HollowHero@users.noreply.github.com>
2019-02-08 00:25:26 +0100

    login fix

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit'])) {
           mysqli_stmt_bind_param($stmt, "s", $username);
           mysqli_stmt_execute($stmt);
           mysqli_stmt_store_result($stmt);
-          $resultCheck = mysqli_query($conn,$sql)
+          $resultCheck = mysqli_query($conn,$sql);
           $row = mysqli_fetch_array($resultCheck, MYSQLI_ASSOC);
           if (password_hash($password, PASSWORD_DEFAULT) == $row["pwd"]){
             header("Location: ../login.php?signup=success")

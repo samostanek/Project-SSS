1d2fc8d79df4838e800af92b1f546d9c95cb962f
Samuel Stanek <samostanek@gmail.com>
2019-03-09 22:57:25 +0100

    Added view for index
    Installed ejs
    Added log for requests

--- server_main.js
+++ server_main.js
@@ -4,6 +4,14 @@ const port = 3000;
 
 const app = express();
 
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  console.log(
+    "Request from: " + req.ip + "and path: " + req.path + " " + req.method
+  );
+});
+
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {

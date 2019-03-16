ce4e27e3caf09f4a6c90bf74b3ad444d21dedbe0
Samuel Stanek <samostanek@gmail.com>
2019-03-16 01:10:45 +0100

    Added story router

--- misc/passport.js
+++ misc/passport.js
@@ -6,40 +6,38 @@ const util = require("./util");
 const User = require("../models/user");
 
 module.exports = passport => {
-  passport.use(
-    new LocalStrategy((email, password, done) => {
-      User.findOne({ $or: [{ mail: email }, { uName: email }] }).then(user => {
-        if (!user) {
-          console.log("Email " + email + " not registered");
-          util.log(
-            "LOGIN",
-            "User tried to login with mail/uName: " + email + ". NOT FOUND"
-          );
-          return done(null, false, { message: "Email not registered." });
-        }
-        // Match Password
-        bcrypt.compare(password, user.pwd, (err, isMatch) => {
-          if (err) throw err;
-          if (isMatch) {
-            console.log("User with email " + email + " logged in.");
-            util.log("LOGIN", "User successfully logged in.", user._id);
-            return done(null, user);
-          } else {
-            console.log("User with email " + email + " has wrong password");
-            util.log("LOGIN", "User has wrong password in.", user._id);
-            return done(null, false, { message: "Password incorrect." });
-          }
-        });
-      });
-    })
-  );
-  passport.serializeUser(function(user, done) {
-    done(null, user.id);
-  });
+	passport.use(
+		new LocalStrategy((email, password, done) => {
+			User.findOne({ $or: [{ mail: email }, { uName: email }] })
+				.then(user => {
+					if (!user) {
+						console.log("Email " + email + " not registered");
+						util.log(
+							"LOGIN",
+							"User tried to login with mail/uName: " + email + ". NOT FOUND"
+						);
+						return done(null, false, { message: "Email not registered." });
+					}
+					// Match Password
+					bcrypt.compare(password, user.pwd, (err, isMatch) => {
+						if (err) throw err;
+						if (isMatch) {
+							console.log("User with email " + email + " logged in.");
+							util.log("LOGIN", "User successfully logged in.", user._id);
+							return done(null, user);
+						} else {
+							console.log("User with email " + email + " has wrong password");
+							util.log("LOGIN", "User has wrong password in.", user._id);
+							return done(null, false, { message: "Password incorrect." });
+						}
+					});
+				})
+				.catch(err => done(err));
+		})
+	);
+	passport.serializeUser((user, done) => done(null, user.id));
 
-  passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-      done(err, user);
-    });
-  });
+	passport.deserializeUser((id, done) =>
+		User.findById(id, (err, user) => done(err, user))
+	);
 };
--- routes/user.js
+++ routes/user.js
@@ -7,75 +7,76 @@ const passport = require("passport");
 const User = require("../models/user");
 
 router.get("/login", (req, res) => {
-  res.render("login", { msgError: req.flash("error") });
+  console.log('test');
+	res.render("login", { msgError: req.flash("error"), msg_success: req.flash("msg_success")});
 });
 router.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "../",
-    failureRedirect: "./login",
-    failureFlash: true
-  })(req, res, next);
+	passport.authenticate("local", {
+		successRedirect: "../",
+		failureRedirect: "./login",
+		failureFlash: true
+	})(req, res, next);
 });
 
 router.get("/logout", (req, res) => {
-  util.log("LOGIN", "User logged out", req.user._id);
-  if (req.body.submit) req.logout();
-  req.flash("success_msg", "You are logged out");
-  res.redirect("/login");
+	util.log("LOGIN", "User logged out", req.user._id);
+	if (req.body.submit) req.logout();
+	req.flash("success_msg", "You are logged out");
+	res.redirect("/login");
 });
 
 router.get("/register", (req, res) =>
-  res.render("register", { errors: null, username: null, email: null })
+	res.render("register", { errors: null, username: null, email: null })
 );
 router.post("/register", (req, res) => {
-  const { username, email, pwd, pwdrpt } = req.body;
+	const { username, email, pwd, pwdrpt } = req.body;
 
-  var errors = [];
-  if (!username || !email || !pwd || !pwdrpt)
-    errors.push({ msg: "Please fill in all fields." });
-  if (pwd !== pwdrpt) errors.push({ msg: "Passwords do not match." });
-  if (pwd.length < 5)
-    errors.push({ msg: "Password must be at least 5 characters" });
-  if (errors.length > 0) {
-    res.render("register", {
-      errors: errors,
-      username: username,
-      email: email
-    });
-  } else {
-    User.findOne({ $or: [{ mail: email }, { uName: username }] })
-      .then(user => {
-        if (user) {
-          // TODO: Differenciate email and username
-          errors.push({ msg: "Email or username already taken." });
-          util.log("REGISTER", "Email or username already taken", user._id);
-          res.render("register", { errors, username, email });
-        } else {
-          const newUser = new User({ uName: username, mail: email, pwd: pwd });
-          // Hash Password
-          bcrypt.genSalt(12, (err, salt) => {
-            if (err) throw error;
-            bcrypt.hash(newUser.pwd, salt, (err, hash) => {
-              if (err) throw error;
-              newUser.pwd = hash;
-              newUser
-                .save()
-                .then(user => {
-                  console.log("User " + username + " registered successfully.");
-                  util.log(
-                    "REGISTER",
-                    "User " + username + " successfully registered."
-                  );
-                  req.flash("msg_success", "You successfully registered!");
-                  res.redirect("/user/login");
-                })
-                .catch(err => console.log(err));
-            });
-          });
-        }
-      })
-      .catch(err => console.log(err));
-  }
+	var errors = [];
+	if (!username || !email || !pwd || !pwdrpt)
+		errors.push({ msg: "Please fill in all fields." });
+	if (pwd !== pwdrpt) errors.push({ msg: "Passwords do not match." });
+	if (pwd.length < 5)
+		errors.push({ msg: "Password must be at least 5 characters" });
+	if (errors.length > 0) {
+		res.render("register", {
+			errors: errors,
+			username: username,
+			email: email
+		});
+	} else {
+		User.findOne({ $or: [{ mail: email }, { uName: username }] })
+			.then(user => {
+				if (user) {
+					// TODO: Differenciate email and username
+					errors.push({ msg: "Email or username already taken." });
+					util.log("REGISTER", "Email or username already taken", user._id);
+					res.render("register", { errors, username, email });
+				} else {
+					const newUser = new User({ uName: username, mail: email, pwd: pwd });
+					// Hash Password
+					bcrypt.genSalt(12, (err, salt) => {
+						if (err) throw error;
+						bcrypt.hash(newUser.pwd, salt, (err, hash) => {
+							if (err) throw error;
+							newUser.pwd = hash;
+							newUser
+								.save()
+								.then(user => {
+									console.log("User " + username + " registered successfully.");
+									util.log(
+										"REGISTER",
+										"User " + username + " successfully registered."
+									);
+									req.flash("msg_success", "You successfully registered!");
+									res.redirect("/user/login");
+								})
+								.catch(err => console.log(err));
+						});
+					});
+				}
+			})
+			.catch(err => console.log(err));
+	}
 });
 
 module.exports = router;
